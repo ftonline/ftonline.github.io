03b8c8d7349ff26aa9c03ece6c87b32895d39d8f
ftonline <35330171+ftonline@users.noreply.github.com>
2018-01-17 20:13:48 +0200

    Add files via upload

--- tournament/tournament.component.js
+++ tournament/tournament.component.js
@@ -4,6 +4,16 @@ angular.
   component('tournament', {
     templateUrl : "tournament/tournament.htm",
     controller : function ($scope, $http, $routeParams) {
-      console.log($routeParams.tournamentId)
+      
+      $scope.cancel = function() {
+				window.location.href = '#!';
+			}
+
+
+      $http.get('https://script.google.com/macros/s/AKfycbyILJZ7cIl5yq0GQycXQHVsuniIZlxUmHVwlwmTEnu86dwNjZvW/exec?func=getTournament&tournamentId=' + $routeParams.tournamentId)
+        .then(function(response){
+          console.log(response.data);
+          $scope.tournament = response.data;
+        });
 	}
 });
