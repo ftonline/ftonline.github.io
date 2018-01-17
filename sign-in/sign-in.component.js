890127b0301230c4a29291cbd4fccc24010c3b14
ftonline <35330171+ftonline@users.noreply.github.com>
2018-01-17 17:31:23 +0200

    Add files via upload

--- sign-in/sign-in.component.js
+++ sign-in/sign-in.component.js
@@ -31,11 +31,6 @@ angular.
 function onSignIn(googleUser) {
   signInScope.isSinedIn = true;
   var profile = googleUser.getBasicProfile();
-  console.log(profile);
-  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  console.log('Name: ' + profile.getName());
-  console.log('Image URL: ' + profile.getImageUrl());
-  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   
   // The ID token you need to pass to your backend:
   var id_token = googleUser.getAuthResponse().id_token;
--- tournaments/tournaments.component.js
+++ tournaments/tournaments.component.js
@@ -4,5 +4,10 @@ angular.
   component('tournaments', {
     templateUrl : "tournaments/tournaments.htm",
     controller : function ($scope, $http) {
+      $http.get('https://script.google.com/macros/s/AKfycbyILJZ7cIl5yq0GQycXQHVsuniIZlxUmHVwlwmTEnu86dwNjZvW/exec?func=getTournaments')
+        .then(function(response){
+          console.log(response.data);
+          $scope.tournaments = response.data;
+        });
 	}
 });
