bb1eecc9b7dbbf88d17e20d1a9893dbe538072d7
ftonline <35330171+ftonline@users.noreply.github.com>
2018-01-23 23:10:05 +0200

    Add files via upload

--- tournament/tournament.component.js
+++ tournament/tournament.component.js
@@ -6,8 +6,21 @@ angular.
     controller : function ($scope, $http, $routeParams) {
       
       $scope.cancel = function() {
-				window.location.href = '#!';
-			}
+        window.location.href = '#!';
+      }
+
+      $scope.registerTournament = function () {
+        var id_token = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().id_token;
+        var data = {token:id_token,func:'registerTournament', tournamentId:$routeParams.tournamentId};
+        
+	console.log(data);
+
+        signInHttp.post('https://script.google.com/macros/s/AKfycbyILJZ7cIl5yq0GQycXQHVsuniIZlxUmHVwlwmTEnu86dwNjZvW/exec', data)
+    	  .then(function(response){
+      	    console.log(response.data);
+	    window.location.href = '#!';
+	   });
+      }
 
 
       $http.get('https://script.google.com/macros/s/AKfycbyILJZ7cIl5yq0GQycXQHVsuniIZlxUmHVwlwmTEnu86dwNjZvW/exec?func=getTournament&tournamentId=' + $routeParams.tournamentId)
