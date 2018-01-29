305bc61183a748e3c7d67b2b7511409ae189c134
ftonline <35330171+ftonline@users.noreply.github.com>
2018-01-29 23:47:34 +0200

    Add files via upload

--- match-game/match-game.component.js
+++ match-game/match-game.component.js
@@ -3,5 +3,26 @@ angular.
   module('matchGame').
   component('matchGame', {
     templateUrl : "match-game/match-game.htm",
-    controller : function ($scope, $http, $routeParams) {}
+    controller : function ($scope, $http, $routeParams) {
+      
+      $scope.cancel = function() {
+        window.location.href = '#!';
+      }
+
+      $scope.getMatchGame = function() {
+        var id_token = gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().id_token;
+        var data = {token:id_token,func:'getMatchGame'};
+        
+	console.log(data);
+
+        $http.post('https://script.google.com/macros/s/AKfycbyILJZ7cIl5yq0GQycXQHVsuniIZlxUmHVwlwmTEnu86dwNjZvW/exec', data)
+    	  .then(function(response){
+      	    console.log(response.data);
+            $scope = response.data.game;
+	  });
+      }
+
+      $scope.getMatchGame();
+
+    }
   });
